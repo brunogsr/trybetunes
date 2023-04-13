13002d4b01c6d501181d8b33825bd6d0a23e51a3
brunogsr <brunogsr https://github.com/brunogsr>
2023-04-13 15:52:55 -0300

    req 1

---
+++ src/pages/Search.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-search">
+        <p>a</p>
+      </div>
+    );
+  }
+}
+
+export default Search;
